0e01bdea3082146f3f593c62fa3f1eeb739ef9c4
minhnln <mingngo158@gmail.com>
2018-02-25 01:56:13 +1000

    add comment

--- src/main/java/com/anonex/codingchallenge/vehiclesurvey/model/Direction.java
+++ src/main/java/com/anonex/codingchallenge/vehiclesurvey/model/Direction.java
@@ -1,5 +1,8 @@
 package com.anonex.codingchallenge.vehiclesurvey.model;
 
+/**
+ * Define direction of vehicle in the survey
+ */
 public enum Direction {
     SOUTH,
     NORTH
--- src/main/java/com/anonex/codingchallenge/vehiclesurvey/model/VehicleEntry.java
+++ src/main/java/com/anonex/codingchallenge/vehiclesurvey/model/VehicleEntry.java
@@ -1,5 +1,11 @@
 package com.anonex.codingchallenge.vehiclesurvey.model;
 
+/**
+ * VehicleEntry class includes all information which can be retrieved from input data
+ *      time when front axle is triggered
+ *      time when rear axle is triggered
+ *      direction of the vehicle
+ */
 public class VehicleEntry {
     private int frontAxleTime;
     private int rearAxleTime;
