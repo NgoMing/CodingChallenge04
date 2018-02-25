f2f8d00a909b8cd93dcd43d4ef6dbf48ea9b0078
minhnln <mingngo158@gmail.com>
2018-02-25 10:02:32 +1000

    Constants of Application

---
+++ src/main/java/com/aconex/codingchallenge/vehiclesurvey/constance/App.java
@@ -0,0 +1,8 @@
+package com.aconex.codingchallenge.vehiclesurvey.constance;
+
+public class App {
+
+    public static final String VALID_REGEX_INPUT = "^[AaBb][0-9]+$";
+    public static final String FIRST_SENSOR_PREFIX = "A";
+    public static final String SECOND_SENSOR_PREFIX = "B";
+}
