524878886a2f7d0c7a82121f958f14b87a423c06
minhnln <mingngo158@gmail.com>
2018-02-26 22:00:58 +1000

    peak count implementation

--- src/main/java/com/aconex/codingchallenge/vehiclesurvey/processor/VehicleCountProcessor.java
+++ src/main/java/com/aconex/codingchallenge/vehiclesurvey/processor/VehicleCountProcessor.java
@@ -1,10 +1,14 @@
 package com.aconex.codingchallenge.vehiclesurvey.processor;
 
 import com.aconex.codingchallenge.vehiclesurvey.constance.App;
+import com.aconex.codingchallenge.vehiclesurvey.constance.Time;
+import com.aconex.codingchallenge.vehiclesurvey.model.Direction;
 import com.aconex.codingchallenge.vehiclesurvey.model.SessionsOfDay;
 import com.aconex.codingchallenge.vehiclesurvey.model.VehicleEntry;
 
+import java.util.Collection;
 import java.util.List;
+import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
 /**
@@ -19,27 +23,47 @@ public class VehicleCountProcessor {
 
     public String process(List<VehicleEntry> entries) {
         StringBuilder sb = new StringBuilder();
+        SessionsOfDay peakSession = null;
+        int peakVehicleCount = 0;
         List<SessionsOfDay> sessions = SessionsOfDay.createSessionsWithInterval(interval);
-        for(SessionsOfDay session : sessions){
-                List<VehicleEntry> sessionEntries = getEntriesInTheSession(entries, session);
-                sb.append(session.toString());
-                for (int day = 0; day < App.NUMBER_OF_DAYS; day++) {
-                        int count = countEntriesForTheDay(sessionEntries,day);
-                        sb.append(" | Day " + day + " Count = " + count);
-                    }
-                sb.append("\n");
+
+        if (sessions.isEmpty())
+            return "";
+
+        for (SessionsOfDay session : sessions) {
+            int totalCountAcrossDay = 0;
+            List<VehicleEntry> sessionEntries = getEntriesInTheSession(entries, session);
+            sb.append(session.toString());
+            for (int day = 0; day < App.NUMBER_OF_DAYS; day++) {
+                int southCount = countEntriesForDayInDirection(sessionEntries, day, Direction.SOUTH);
+                int northCount = countEntriesForDayInDirection(sessionEntries, day, Direction.NORTH);
+                sb.append(" | Day ").append(day).append(" Count in South = ").append(southCount).append(" in North = ").append(northCount);
+                totalCountAcrossDay += southCount + northCount;
             }
+            sb.append("\n");
+            if (peakVehicleCount < totalCountAcrossDay) {
+                peakVehicleCount = totalCountAcrossDay;
+                peakSession = session;
+            }
+        }
+
+        sb.append('\n').append(peakSession).append(" is peak session with ").append(peakVehicleCount).append(" cars");
+
         return sb.toString();
     }
 
-    private int countEntriesForTheDay(List<VehicleEntry> sessionEntries, int day) {
-        return sessionEntries.stream().filter(entry -> entry.getDay() == day).collect(Collectors.toList()).size();
+    private int countEntriesForDayInDirection(List<VehicleEntry> sessionEntries, int day, Direction direction) {
+        return sessionEntries.stream().
+                filter(entry -> entry.getDay() == day && entry.getDirection() == direction).
+                collect(Collectors.toList()).size();
     }
 
     private List<VehicleEntry> getEntriesInTheSession(List<VehicleEntry> entries, SessionsOfDay session) {
         return entries.stream().
-                        filter(entry -> entry.getTimeEntry().compareTo(session.getStartTime()) >= 0
-                                && entry.getTimeEntry().compareTo(session.getEndTime()) < 0)
-                        .collect(Collectors.toList());
+                filter(entry -> entry.getTimeEntry().compareTo(session.getStartTime()) >= 0
+                             && entry.getTimeEntry().compareTo(session.getEndTime()) <= 0).
+                collect(Collectors.toList());
     }
+
+
 }
--- src/test/java/com/aconex/codingchallenge/vehiclesurvey/processor/VehicleCountProcessorTest.java
+++ src/test/java/com/aconex/codingchallenge/vehiclesurvey/processor/VehicleCountProcessorTest.java
@@ -15,30 +15,31 @@ public class VehicleCountProcessorTest {
     private List<VehicleEntry> entries = new ArrayList<>();
 
     private String expectedOutput =
-        "Session 00:00:00 to 01:00:00 | Day 0 Count=3 | Day 1 Count=0 | Day 2 Count=0 | Day 3 Count=0 | Day 4 Count=0\n" +
-        "Session 01:00:00 to 02:00:00 | Day 0 Count=1 | Day 1 Count=2 | Day 2 Count=1 | Day 3 Count=0 | Day 4 Count=0\n" +
-        "Session 02:00:00 to 03:00:00 | Day 0 Count=0 | Day 1 Count=0 | Day 2 Count=1 | Day 3 Count=1 | Day 4 Count=1\n" +
-        "Session 03:00:00 to 04:00:00 | Day 0 Count=0 | Day 1 Count=0 | Day 2 Count=0 | Day 3 Count=0 | Day 4 Count=0\n" +
-        "Session 04:00:00 to 05:00:00 | Day 0 Count=0 | Day 1 Count=0 | Day 2 Count=0 | Day 3 Count=0 | Day 4 Count=0\n" +
-        "Session 05:00:00 to 06:00:00 | Day 0 Count=0 | Day 1 Count=0 | Day 2 Count=0 | Day 3 Count=0 | Day 4 Count=0\n" +
-        "Session 06:00:00 to 07:00:00 | Day 0 Count=0 | Day 1 Count=0 | Day 2 Count=0 | Day 3 Count=0 | Day 4 Count=0\n" +
-        "Session 07:00:00 to 08:00:00 | Day 0 Count=0 | Day 1 Count=0 | Day 2 Count=0 | Day 3 Count=0 | Day 4 Count=0\n" +
-        "Session 08:00:00 to 09:00:00 | Day 0 Count=0 | Day 1 Count=0 | Day 2 Count=0 | Day 3 Count=0 | Day 4 Count=0\n" +
-        "Session 09:00:00 to 10:00:00 | Day 0 Count=0 | Day 1 Count=0 | Day 2 Count=0 | Day 3 Count=0 | Day 4 Count=0\n" +
-        "Session 10:00:00 to 11:00:00 | Day 0 Count=0 | Day 1 Count=0 | Day 2 Count=0 | Day 3 Count=0 | Day 4 Count=0\n" +
-        "Session 11:00:00 to 12:00:00 | Day 0 Count=0 | Day 1 Count=0 | Day 2 Count=0 | Day 3 Count=0 | Day 4 Count=0\n" +
-        "Session 12:00:00 to 13:00:00 | Day 0 Count=0 | Day 1 Count=0 | Day 2 Count=0 | Day 3 Count=0 | Day 4 Count=0\n" +
-        "Session 13:00:00 to 14:00:00 | Day 0 Count=0 | Day 1 Count=0 | Day 2 Count=0 | Day 3 Count=0 | Day 4 Count=0\n" +
-        "Session 14:00:00 to 15:00:00 | Day 0 Count=0 | Day 1 Count=0 | Day 2 Count=0 | Day 3 Count=0 | Day 4 Count=0\n" +
-        "Session 15:00:00 to 16:00:00 | Day 0 Count=0 | Day 1 Count=0 | Day 2 Count=0 | Day 3 Count=0 | Day 4 Count=0\n" +
-        "Session 16:00:00 to 17:00:00 | Day 0 Count=0 | Day 1 Count=0 | Day 2 Count=0 | Day 3 Count=0 | Day 4 Count=0\n" +
-        "Session 17:00:00 to 18:00:00 | Day 0 Count=0 | Day 1 Count=0 | Day 2 Count=0 | Day 3 Count=0 | Day 4 Count=0\n" +
-        "Session 18:00:00 to 19:00:00 | Day 0 Count=0 | Day 1 Count=0 | Day 2 Count=0 | Day 3 Count=0 | Day 4 Count=0\n" +
-        "Session 19:00:00 to 20:00:00 | Day 0 Count=0 | Day 1 Count=0 | Day 2 Count=0 | Day 3 Count=0 | Day 4 Count=0\n" +
-        "Session 20:00:00 to 21:00:00 | Day 0 Count=0 | Day 1 Count=0 | Day 2 Count=0 | Day 3 Count=0 | Day 4 Count=0\n" +
-        "Session 21:00:00 to 22:00:00 | Day 0 Count=0 | Day 1 Count=0 | Day 2 Count=0 | Day 3 Count=0 | Day 4 Count=0\n" +
-        "Session 22:00:00 to 23:00:00 | Day 0 Count=0 | Day 1 Count=0 | Day 2 Count=0 | Day 3 Count=0 | Day 4 Count=0\n" +
-        "Session 23:00:00 to 00:00:00 | Day 0 Count=0 | Day 1 Count=0 | Day 2 Count=0 | Day 3 Count=0 | Day 4 Count=0\n";
+        "Session 00:00:00 to 01:00:00 | Day 0 Count in South = 3 in North = 0 | Day 1 Count in South = 0 in North = 0 | Day 2 Count in South = 0 in North = 0 | Day 3 Count in South = 0 in North = 0 | Day 4 Count in South = 0 in North = 0\n" +
+        "Session 01:00:00 to 02:00:00 | Day 0 Count in South = 1 in North = 0 | Day 1 Count in South = 2 in North = 0 | Day 2 Count in South = 1 in North = 0 | Day 3 Count in South = 0 in North = 0 | Day 4 Count in South = 0 in North = 0\n" +
+        "Session 02:00:00 to 03:00:00 | Day 0 Count in South = 0 in North = 0 | Day 1 Count in South = 0 in North = 0 | Day 2 Count in South = 1 in North = 0 | Day 3 Count in South = 1 in North = 0 | Day 4 Count in South = 1 in North = 0\n" +
+        "Session 03:00:00 to 04:00:00 | Day 0 Count in South = 0 in North = 0 | Day 1 Count in South = 0 in North = 0 | Day 2 Count in South = 0 in North = 0 | Day 3 Count in South = 0 in North = 0 | Day 4 Count in South = 0 in North = 0\n" +
+        "Session 04:00:00 to 05:00:00 | Day 0 Count in South = 0 in North = 0 | Day 1 Count in South = 0 in North = 0 | Day 2 Count in South = 0 in North = 0 | Day 3 Count in South = 0 in North = 0 | Day 4 Count in South = 0 in North = 0\n" +
+        "Session 05:00:00 to 06:00:00 | Day 0 Count in South = 0 in North = 0 | Day 1 Count in South = 0 in North = 0 | Day 2 Count in South = 0 in North = 0 | Day 3 Count in South = 0 in North = 0 | Day 4 Count in South = 0 in North = 0\n" +
+        "Session 06:00:00 to 07:00:00 | Day 0 Count in South = 0 in North = 0 | Day 1 Count in South = 0 in North = 0 | Day 2 Count in South = 0 in North = 0 | Day 3 Count in South = 0 in North = 0 | Day 4 Count in South = 0 in North = 0\n" +
+        "Session 07:00:00 to 08:00:00 | Day 0 Count in South = 0 in North = 0 | Day 1 Count in South = 0 in North = 0 | Day 2 Count in South = 0 in North = 0 | Day 3 Count in South = 0 in North = 0 | Day 4 Count in South = 0 in North = 0\n" +
+        "Session 08:00:00 to 09:00:00 | Day 0 Count in South = 0 in North = 0 | Day 1 Count in South = 0 in North = 0 | Day 2 Count in South = 0 in North = 0 | Day 3 Count in South = 0 in North = 0 | Day 4 Count in South = 0 in North = 0\n" +
+        "Session 09:00:00 to 10:00:00 | Day 0 Count in South = 0 in North = 0 | Day 1 Count in South = 0 in North = 0 | Day 2 Count in South = 0 in North = 0 | Day 3 Count in South = 0 in North = 0 | Day 4 Count in South = 0 in North = 0\n" +
+        "Session 10:00:00 to 11:00:00 | Day 0 Count in South = 0 in North = 0 | Day 1 Count in South = 0 in North = 0 | Day 2 Count in South = 0 in North = 0 | Day 3 Count in South = 0 in North = 0 | Day 4 Count in South = 0 in North = 0\n" +
+        "Session 11:00:00 to 12:00:00 | Day 0 Count in South = 0 in North = 0 | Day 1 Count in South = 0 in North = 0 | Day 2 Count in South = 0 in North = 0 | Day 3 Count in South = 0 in North = 0 | Day 4 Count in South = 0 in North = 0\n" +
+        "Session 12:00:00 to 13:00:00 | Day 0 Count in South = 0 in North = 0 | Day 1 Count in South = 0 in North = 0 | Day 2 Count in South = 0 in North = 0 | Day 3 Count in South = 0 in North = 0 | Day 4 Count in South = 0 in North = 0\n" +
+        "Session 13:00:00 to 14:00:00 | Day 0 Count in South = 0 in North = 0 | Day 1 Count in South = 0 in North = 0 | Day 2 Count in South = 0 in North = 0 | Day 3 Count in South = 0 in North = 0 | Day 4 Count in South = 0 in North = 0\n" +
+        "Session 14:00:00 to 15:00:00 | Day 0 Count in South = 0 in North = 0 | Day 1 Count in South = 0 in North = 0 | Day 2 Count in South = 0 in North = 0 | Day 3 Count in South = 0 in North = 0 | Day 4 Count in South = 0 in North = 0\n" +
+        "Session 15:00:00 to 16:00:00 | Day 0 Count in South = 0 in North = 0 | Day 1 Count in South = 0 in North = 0 | Day 2 Count in South = 0 in North = 0 | Day 3 Count in South = 0 in North = 0 | Day 4 Count in South = 0 in North = 0\n" +
+        "Session 16:00:00 to 17:00:00 | Day 0 Count in South = 0 in North = 0 | Day 1 Count in South = 0 in North = 0 | Day 2 Count in South = 0 in North = 0 | Day 3 Count in South = 0 in North = 0 | Day 4 Count in South = 0 in North = 0\n" +
+        "Session 17:00:00 to 18:00:00 | Day 0 Count in South = 0 in North = 0 | Day 1 Count in South = 0 in North = 0 | Day 2 Count in South = 0 in North = 0 | Day 3 Count in South = 0 in North = 0 | Day 4 Count in South = 0 in North = 0\n" +
+        "Session 18:00:00 to 19:00:00 | Day 0 Count in South = 0 in North = 0 | Day 1 Count in South = 0 in North = 0 | Day 2 Count in South = 0 in North = 0 | Day 3 Count in South = 0 in North = 0 | Day 4 Count in South = 0 in North = 0\n" +
+        "Session 19:00:00 to 20:00:00 | Day 0 Count in South = 0 in North = 0 | Day 1 Count in South = 0 in North = 0 | Day 2 Count in South = 0 in North = 0 | Day 3 Count in South = 0 in North = 0 | Day 4 Count in South = 0 in North = 0\n" +
+        "Session 20:00:00 to 21:00:00 | Day 0 Count in South = 0 in North = 0 | Day 1 Count in South = 0 in North = 0 | Day 2 Count in South = 0 in North = 0 | Day 3 Count in South = 0 in North = 0 | Day 4 Count in South = 0 in North = 0\n" +
+        "Session 21:00:00 to 22:00:00 | Day 0 Count in South = 0 in North = 0 | Day 1 Count in South = 0 in North = 0 | Day 2 Count in South = 0 in North = 0 | Day 3 Count in South = 0 in North = 0 | Day 4 Count in South = 0 in North = 0\n" +
+        "Session 22:00:00 to 23:00:00 | Day 0 Count in South = 0 in North = 0 | Day 1 Count in South = 0 in North = 0 | Day 2 Count in South = 0 in North = 0 | Day 3 Count in South = 0 in North = 0 | Day 4 Count in South = 0 in North = 0\n" +
+        "Session 23:00:00 to 00:00:00 | Day 0 Count in South = 0 in North = 0 | Day 1 Count in South = 0 in North = 0 | Day 2 Count in South = 0 in North = 0 | Day 3 Count in South = 0 in North = 0 | Day 4 Count in South = 0 in North = 0\n\n" +
+        "Session 01:00:00 to 02:00:00 is peak session with 4 cars";
 
     @Before
     public void setUp() throws Exception {
